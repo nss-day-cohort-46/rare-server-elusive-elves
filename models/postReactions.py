3df10c7f60c3b1fec1edbb9a1b9546c64836c414
nathanRomad <nathan.a.hamilton@gmail.com>
2021-04-26 14:13:56 -0500

    feat: added files, started posts/request.py

--- models/postReactions.py
+++
@@ -1,6 +0,0 @@
-class PostReactions():
-    def __init__(self, id, user_id, post_id, reaction_id):
-        self.id = id
-        self.user_id = user_id
-        self.post_id = post_id
-        self.reaction_id = reaction_id
